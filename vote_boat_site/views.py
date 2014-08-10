8e006d79284124128d0e93898905f16b374fd812
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-08-10 13:15:30 -0600

    added home page views to the vote boat

--- vote_boat_site/views.py
+++ vote_boat_site/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from vote_boat.views import create_new_poll
+
+
 def home (request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
@@ -10,9 +13,19 @@ def home (request):
     
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = dict(boldmessage="edit content")
+    context_dict = dict(message="Welcome to...the Vote Boat!")
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render_to_response('vote_boat_site/index.html', context_dict, context)
+
+def thank_you (request):
+    context = RequestContext(request)
+    context_dict = dict(message="Thank You!")
+    return render_to_response('vote_boat_site/index.html', context_dict, context)
+    
+new_poll = create_new_poll('vote_boat_site/new_poll.html',thank_you)
+
+
+
